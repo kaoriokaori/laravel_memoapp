3920e30dfdd788fed1bf35bf1a72988e192afc31
kor-imac <k.tanigawa.813@gmail.com>
2022-11-22 22:23:30 +0900

    aaa

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,7 +7,7 @@
 @section('content')
     <div class="card">
         <div class="card-header d-flex justify-content-between">
-            メモ編集
+            メモ編集aaa
             <form id="delete-form" action="{{ route('destory') }}" method="POST">
                 @csrf
                 <input type="hidden" name="memo_id" value="{{ $edit_memo[0]['id'] }}" />
